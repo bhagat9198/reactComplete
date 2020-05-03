768276fbede46091f004320c3d808cce4c4af8c4
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-05-03 22:03:23 +0530

    3.14state 3.16 button

--- src/App.js
+++ src/App.js
@@ -22,19 +22,51 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // properties in class is eaqual to defining varaibles.
+
+  // as we are using the class, w can define variables directly
+  // var myVar = 'hello'; //error
+  myVaraiable = 'Something';
+  myNumber = 10;
+  // thus no need of using 'var', 'const', 'let'.
+
+  // as we are extending Component class, it gives us one special keyword 'state' 
+  // params : use to set and pass from outside
+  // state : it is managed from inside the componenet
+    // in new version of react, we can manage from inside the functional component
+    // we should not use 'state' as  much as possible as it makes our app unpredicataiable and we will not able to handle it properly.
+  // state is pecial property : which means that if value inside state is changed, it will make react to rerender DOM automatically
+  
+  state = {
+    person : [
+      {name:'Max', age:'29'},
+      {name:'Bob', age:'19'},
+      {name:'Mark', age:'15'},
+    ]
+  }
+
+  // "switchName" is the fuction name + "handler": its specify that this method is not activaly called by user, but its an event handler 
+  switchNameHandler = () => {
+    console.log('clicked!!');
+  }
+
+  // its method provided by react
   render() {
     return (
       <div className="App">
         <h1>Hello world of React!!!</h1>
-        <Person />
-        {/* if we want to pass our own data which can we displayed. we will pass those values as agruments to that componenent. */}
-        <Person name="Max" age="30"/>
-        <Person  name="Bob" age="19"/>
-        <Person name="Mark" age="50"/>
+        {/* passing dynamic value */}
+        {/* here, 'this' refferes to present class ie 'App' class */}
+        <Person  name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}> My Hobbies : Soccer, Cricket </Person>
 
-        {/* passing extra data(html) other than passing argument */}
-        <Person name="alex" age="25"> My Hobbies are : Soccer </Person>
+        {/* adding event handler : if user clicks on button, method should execute
+        in react we use "onClick" instaed of "onclick" which is normal way in JS
+        */}
+        <button  onClick={this.switchNameHandler}>Switch Names</button>
       </div>
+
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-// it accept only one agrument
 const person = (props) => {
-  // as 'App' is passing agruments as key value pair. keys are 'name' and 'age'
   return (
     <div>
       <p>Hello I am {props.name} and having age of {props.age} </p>
-      {/* if we want to display some extra html, that can be extracted with help of 'children' keyword.*/}
       <span>{props.children}</span>
     </div>
   );
