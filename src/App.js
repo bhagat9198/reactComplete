5e2d7dacf7dc9e147bdc5c369c466f3592e3f8e5
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-05-04 11:39:58 +0530

    3.18 Manipulating the State

--- src/App.js
+++ src/App.js
@@ -16,54 +16,48 @@
 
 import React, {Component} from 'react';
 import './App.css';
-
-// import the Person componenent
-// no need to mention '.js' extension. 
 import Person from './Person/Person';
 
 class App extends Component {
-  // properties in class is eaqual to defining varaibles.
-
-  // as we are using the class, w can define variables directly
-  // var myVar = 'hello'; //error
-  myVaraiable = 'Something';
-  myNumber = 10;
-  // thus no need of using 'var', 'const', 'let'.
-
-  // as we are extending Component class, it gives us one special keyword 'state' 
-  // params : use to set and pass from outside
-  // state : it is managed from inside the componenet
-    // in new version of react, we can manage from inside the functional component
-    // we should not use 'state' as  much as possible as it makes our app unpredicataiable and we will not able to handle it properly.
-  // state is pecial property : which means that if value inside state is changed, it will make react to rerender DOM automatically
   
   state = {
     person : [
       {name:'Max', age:'29'},
       {name:'Bob', age:'19'},
       {name:'Mark', age:'15'},
-    ]
+    ],
+    something: 'hello'
   }
 
-  // "switchName" is the fuction name + "handler": its specify that this method is not activaly called by user, but its an event handler 
   switchNameHandler = () => {
-    console.log('clicked!!');
+    // console.log('clicked!!');
+    console.log(this.state.something);
+
+    // changing the value
+    // this is not the recommened way to change the state value. hence it will not work
+    // this.state.person[0].name = 'Alex';
+
+    // 'setState': its a inbuild method provided by Component class. with this method only, we can change the values of 'State'
+    this.setState(
+      // it takes an object, same as we do in 'state' and pass the keys whose value we want to change.
+      {
+        // here we are passing only 'person' as we are chaging its value.
+        person : [
+          {name:'Alex', age:'29'},
+          {name:'Bob', age:'19'},
+          {name:'Mark', age:'55'},
+        ]
+      }
+    );
   }
 
-  // its method provided by react
   render() {
     return (
       <div className="App">
         <h1>Hello world of React!!!</h1>
-        {/* passing dynamic value */}
-        {/* here, 'this' refferes to present class ie 'App' class */}
         <Person  name={this.state.person[0].name} age={this.state.person[0].age}/>
         <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}> My Hobbies : Soccer, Cricket </Person>
-
-        {/* adding event handler : if user clicks on button, method should execute
-        in react we use "onClick" instaed of "onclick" which is normal way in JS
-        */}
         <button  onClick={this.switchNameHandler}>Switch Names</button>
       </div>
 
