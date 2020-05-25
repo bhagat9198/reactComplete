577368fdd2a87a26a590eeba129a928c55cc8910
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-05-25 22:10:09 +0530

    4.2. Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       {name:'Bob', age:'19'},
       {name:'Mark', age:'15'},
     ],
-    something: 'hello'
+    something: 'hello',
+    showPersons: false
   }
  
   switchNameHandler = (newName) => {
@@ -24,6 +25,22 @@ class App extends Component {
       }
     );
   };
+
+  // we can crete a 'method' like too as we are within the class. but it can lead to problems when this event is fired in DOM.
+  // toggelPersonsHandler() {  }
+
+  // but, we will use arrow function so that we know 'this' keyword always refferes to class
+  // as we are within the class, no need to write 'const' or 'let'
+  toggelPersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    // getting the 'boolena' value of 'showPersons'. now changing the boolean value and setting the state.
+    this.setState({
+      showPersons: !doesShow
+    });
+    // thus, only showPerson state only changed, rest all state will be same as it is. 
+  };
+
+  // in inspect section also we can see, on pressing the button 'div' element which is holding Person compontent values will goes off and comes back. hence, elemnts are really rendered.  
   
   nameChangedHandler = (event) => {
     this.setState(
@@ -39,8 +56,6 @@ class App extends Component {
   
 
   render() {
-    // Inline style is nothing but styling in JS way.
-    // thus creating an objecta and passing the values in key-value pair
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
@@ -49,35 +64,49 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    // thus, inline css is helpful when we want to style a specific element only. as if we apply style in 'css' file, it will have gloval scope.
-    // inline has some limitation too, like we cant style button for hover effect. 
-
     return (
       <div className="App">
         <h1>Hello world of React!!!</h1>
         <button  
-          // passing style property. here 'Style' is keyword which means it holds only css properties
-          // style1={style} //error: although passing css properties but will not get applied
           style={style}
-          onClick={this.switchNameHandler.bind(this, 'Dark')}>Switch Names
-        </button>
-        <Person  
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-        />
-        <Person 
-          name={this.state.person[1].name} 
-          age={this.state.person[1].age}
-          changed={this.nameChangedHandler}
-        />
-        <Person 
-          name={this.state.person[2].name} 
-          age={this.state.person[2].age}
-          click={this.switchNameHandler.bind(this, 'Alex')}> My Hobbies : Soccer, Cricket  
-        </Person>
-        
-      </div>
+          // onClick={this.switchNameHandler.bind(this, 'Dark')}>Switch Names
 
+          // change of functionality,
+          // now we want persons to get hide and show vise versa(toggle) when button is clicked
+          onClick={this.toggelPersonsHandler.bind(this)}>Switch Names
+
+        </button>
+        {/* wrapping all the persons within one div */}
+        {/* we know that react works on pure JS, thus to put a condition(or any logic) in JSX, we can wrap our JSX within {} */}
+
+        {
+          // after warpping it {}, it became out JSS code. and hance put condition
+          // if(this.state.showPersons) //Error: we cant put block conditions within JSX
+          // hence, putting ternary operator
+
+          this.state.showPersons ?
+            // if condition is true. its JS, not html. atlast it will be converted into React.CreateElement()..... hence, it make perfect sense 
+            <div>
+              <Person  
+                name={this.state.person[0].name}
+                age={this.state.person[0].age}
+              />
+              <Person 
+                name={this.state.person[1].name} 
+                age={this.state.person[1].age}
+                changed={this.nameChangedHandler}
+              />
+              <Person 
+                name={this.state.person[2].name} 
+                age={this.state.person[2].age}
+                click={this.switchNameHandler.bind(this, 'Alex')}> My Hobbies : Soccer, Cricket  
+              </Person>
+            </div> 
+            :
+            // if condition is false
+            null
+        }
+      </div>
     );
   }
 }
