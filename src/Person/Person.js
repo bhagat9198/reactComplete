28dff110c632192f826194a0b82989a05fe5f48e
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-05-25 19:50:04 +0530

    23. Adding Two Way Binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,28 +4,17 @@ const person = (props) => {
   return (
     <div>
       <p>Hello I am {props.name} and having age of {props.age} </p>
-      {/* passing onClick method and linking 'switchNameHandler'  */}
-      <span onClick={props.click} >{props.children}</span>
+      {/* for better indentation, changing 'span' to 'div' */}
+      <p onClick={props.click} >{props.children}</p>
+      {/* putting the inputbox, so that user can enter the text */}
+      {/* onChanged method will be triggered whenever user enter text in inputbox and hence state is changed. */}
+      {/* <input type="text" onChange={props.changed} value={props.name} /> */}
+
+      {/* we also want intially, textbox should contain the value as person name. */}
+      {/* This is known as two way binding, we are passing the text box data as well as getting the data */}
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
 
 export default person;
-
-
-
-// using class
-// import React, {Component} from 'react';
-
-// class Person extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <p>Hello I am {this.props.name} and having age of {this.props.age} 
-//           <span>{this.props.children}</span>
-//         </p>
-//       </div>
-//     )
-//   }
-// }
-// export default Person;
