b761b1357e6d0addda0f0a115cc0baf15e341a82
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-05-26 09:43:06 +0530

    4.3. Handling Dynamic Content The JavaScript Way

--- src/App.js
+++ src/App.js
@@ -26,22 +26,13 @@ class App extends Component {
     );
   };
 
-  // we can crete a 'method' like too as we are within the class. but it can lead to problems when this event is fired in DOM.
-  // toggelPersonsHandler() {  }
-
-  // but, we will use arrow function so that we know 'this' keyword always refferes to class
-  // as we are within the class, no need to write 'const' or 'let'
   toggelPersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    // getting the 'boolena' value of 'showPersons'. now changing the boolean value and setting the state.
     this.setState({
       showPersons: !doesShow
     });
-    // thus, only showPerson state only changed, rest all state will be same as it is. 
   };
 
-  // in inspect section also we can see, on pressing the button 'div' element which is holding Person compontent values will goes off and comes back. hence, elemnts are really rendered.  
-  
   nameChangedHandler = (event) => {
     this.setState(
       {
@@ -54,7 +45,7 @@ class App extends Component {
     );
   };
   
-
+  // whenever the elements are rendered to DOM, whole rendered method is executed not just return method inside render method. hece, we can write JS within the render method which needed to be executed everytime whenever we want to render elements to DOM.  
   render() {
     const style = {
       backgroundColor: 'white',
@@ -64,49 +55,39 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    // its no a good practice to write conditions within JSX, hence followig different approach.
+    let persons = null;
+    // putting the if condition and passing JSX
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person  
+            name={this.state.person[0].name}
+            age={this.state.person[0].age}
+          />
+          <Person 
+            name={this.state.person[1].name} 
+            age={this.state.person[1].age}
+            changed={this.nameChangedHandler}
+          />
+          <Person 
+            name={this.state.person[2].name} 
+            age={this.state.person[2].age}
+            click={this.switchNameHandler.bind(this, 'Alex')}> My Hobbies : Soccer, Cricket  
+          </Person>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello world of React!!!</h1>
         <button  
           style={style}
-          // onClick={this.switchNameHandler.bind(this, 'Dark')}>Switch Names
-
-          // change of functionality,
-          // now we want persons to get hide and show vise versa(toggle) when button is clicked
           onClick={this.toggelPersonsHandler.bind(this)}>Toggele Persons
-
         </button>
-        {/* wrapping all the persons within one div */}
-        {/* we know that react works on pure JS, thus to put a condition(or any logic) in JSX, we can wrap our JSX within {} */}
-
-        {
-          // after warpping it {}, it became out JSS code. and hance put condition
-          // if(this.state.showPersons) //Error: we cant put block conditions within JSX
-          // hence, putting ternary operator
-
-          this.state.showPersons ?
-            // if condition is true. its JS, not html. atlast it will be converted into React.CreateElement()..... hence, it make perfect sense 
-            <div>
-              <Person  
-                name={this.state.person[0].name}
-                age={this.state.person[0].age}
-              />
-              <Person 
-                name={this.state.person[1].name} 
-                age={this.state.person[1].age}
-                changed={this.nameChangedHandler}
-              />
-              <Person 
-                name={this.state.person[2].name} 
-                age={this.state.person[2].age}
-                click={this.switchNameHandler.bind(this, 'Alex')}> My Hobbies : Soccer, Cricket  
-              </Person>
-            </div> 
-            :
-            // if condition is false
-            null
-        }
-        {/* this is doing our work which we wanted, but this JSX can be confusing once it become large. */}
+        {/* just passing the dynamic value of persons which can be null or have Person component. */}
+        {persons}
       </div>
     );
   }
