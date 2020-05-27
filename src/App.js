3fb4c661cfb4f8111fab67954b160a37e4e0b6b3
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-05-27 17:28:12 +0530

    5.5 and 5.6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+
+// to use radium, first we need to import it
+// Radium is defualt export, and then we imported 'styleRoot' named export  
+import Radium, { StyleRoot } from 'radium';
+
 import './App.css';
 import Person from './Person/Person';
 
@@ -42,17 +47,19 @@ class App extends Component {
     this.setState({persons: persons});
   }
   
-  // btn should have green bg when persons are hidden and 'red' bg when persons are shown.
   render() {
     const style = {
-      // 
       backgroundColor: 'green',
       color: 'white',
-
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      // now within the style we can add the sudo selector. but it should be wrapped in singke quotes ie like a string. this is because ':' is not a valid JS code. and its properties will be wrapped within object.
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -72,40 +79,46 @@ class App extends Component {
           })}
         </div>
       );
-      // it will be only executed when persons are shown
-      // changing the property of object
       style.backgroundColor = 'red';
+      // adding over property
+      // we know we can extract the object values in array notation also.
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+
     }
 
-    // adding classes dynamically
-    // we want "this is really working" should add style dynamically depending upo the number of persons in the list
-    // defining two classes in App.css : red and bold
-
-    // inilizing an array which can store the class names
     let classes = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      classes.push('red'); 
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold');  // classes = ['red', 'bold]
+      classes.push('bold');  
     }
 
     return (
-      <div className="App">
-        <h1>Hello world of React!!!</h1>
-        {/* everything is JS, thus we can add classes also dynamically by putting {} */}
-        {/* now we have add an array which can contain both classes or single class or none of the class. className accepts a string not an array, hence we are converting array into  string with help of 'join()' method */}
-        {/* join(' ') : Adds all the elements of an array separated by the specified separator string. here one space is the seprator field */}
-        <p className={classes.join(' ')}>This is really working.</p>
-        <button  
-          style={style}
-          onClick={this.toggelPersonsHandler.bind(this)}>Toggele Persons
-        </button>
-        {persons}
-      </div>
+      // as we are using media query in form of inline css, we need to wrap or application by special component provided by radium.
+      // wrapping our whole component which is rendered within <StyleRoot>
+      < StyleRoot>
+        <div className="App">
+          <h1>Hello world of React!!!</h1>
+          <p className={classes.join(' ')}>This is really working.</p>
+          <button  
+            style={style}
+            onClick={this.toggelPersonsHandler.bind(this)}>Toggele Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
-export default App;
+
+// Wraping the App in radium function. this is known as higher order componenet. 
+// higher order componenet :  a componenet which is wrapping one more component within it. thus. adding extra functionalities to our component.
+// it can be used on both class and funtional componenets.
+
+export default Radium(App);
 
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import './Person.css'
+
+// importing 
+import Radium from 'radium';
+
+import './Person.css';
 
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)' : {
+      width: '450px'
+    }
+  }
+
   return (
-    <div className="Person">
+    // inline styles overwrite the class styles as they are more specific. its a default css rule not radium rule.
+    <div className="Person" style={style} >
       <p onClick={props.click}>Hello I am {props.name} and having age of {props.age} </p>
       <p >{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
@@ -11,4 +22,4 @@ const person = (props) => {
   );
 };
 
-export default person;
+export default Radium(person);
