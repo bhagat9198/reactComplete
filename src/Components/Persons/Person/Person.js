22808e66d786e0a0da5f89febe07eb70a23ffcf8
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-08 20:20:00 +0530

    7.26. Using PropTypes

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -17,16 +17,28 @@
 
 
 
+
+
+// here, we are having number of user props which holds particular data type like string, number, etc. and 'children' is speacial type of prop.
+// if lots of ppl are working on our code and it can happen they pass wrong data type. it will be good if we can specify what type of data particular prop uses.
+// to do this, we will 3rd party tool
+  // npm install --save prop-types
+// it can be used with functional and class based components.
+
 import React, { Component} from 'react';
 import Auxilliary from '../../../hoc/Auxilliary'
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 
+// requiring 3rs party
+import PropTypes from 'prop-types';
+
 class Person extends Component {
   
   render() {
     console.log('[Person.js] render...');
-    console.log(this.props);
+    // console.log(this.props);
+
     return (
       <Auxilliary>
         <p onClick={this.props.click}>
@@ -42,6 +54,17 @@ class Person extends Component {
       </Auxilliary>
     );
   }
+};
+
+// using PropTypes
+// <componentName>.propTypes = {} : 'propTypes' is inbuild special property which we can add to any Js componenet object and react will watch out for in developement mode and give us the warning if wrong datatype of data is passed in props. 
+Person.propTypes = {
+  // passing key: value pair where 'key' is prop name and 'value' will be datatype of prop
+  click : PropTypes.func,
+  name : PropTypes.string,
+  age : PropTypes.number,
+  changed : PropTypes.func
+  // PropTypes can be advaced also where we cav define which functio acception how many argumenst and of what type.
 };
 
 export default withClass(Person, classes.Person);
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -64,37 +64,13 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    // till now we are using state correctly.
-    // lets see how we can use it incorrectly. 
-    // for that, let add a counter which count the key strokes made when name is changed and total keys strokes should be stored.
-
-    // we know, this.setSate will be triggered whenever namechange is done ie with every keystroke name is changed and everytime name will be changed. 
-    // updating the counter : it depends upon previous state
-    // this.setState({persons: persons, changeCounter: this.state.changeCounter + 1});
-    // our logic and app is working fine but its a wrong way of setting up the state.
-
-    // behiend the sceans, setState does not immediately trgger instead its is basically secheduled by react and react will then perform the state update and the re-render cycle when it has available resources to do that.
-    // we are calling set state synchronously but doesnt gurantees to execute and finish immediately. thus in above "this.state.changeCounter" cant be latest state everytime.
-    // hence, when our new state state depends upon old state. we can use the above technnique.
-
-
-    // setStae does not only take Js obj. 
-    // it also works when we pass a function. it will take 2 args :
-      // 1arg: previous state
-      // 2arg: current props
     this.setState((prevState, props) => {
-      // in fun body, we should return the new JS obj
       return {
         persons: persons,
-        // changeCounter: this.state.changeCounter + 1
         changeCounter: prevState.changeCounter + 1
       }
     }) 
-    // and here react eill gurantee that new state was updated on last state only
-
   };
-
-  
 
   deletePersonHandler = (index) => {
     const persons = [...this.state.persons];
