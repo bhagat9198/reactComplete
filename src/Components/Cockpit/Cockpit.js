3faaec51959ee38f73167cc81d45ae102b7bf5dd
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-09 10:58:05 +0530

    7.28. Refs with React Hooks

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react';
+// we want 'toogle button' should be prssed automatically when the application loads for the first time. ie person cards should be visible.
+
+// importing the react hook for handling ref
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const Cockpit = (props) => {
+  // setting up the reference
+  // as we are in function, we cant put a constructor. so setting up another function. this function should be before the JSX return
+  // const toggleButtonRef = React.createRef // cant use, it only for class
+  // using react hook ' useRef'
+  // userRef() :  we can pass the initial values bec 
+    // 1 : it is not only used for refferences, to access to DOM element
+    // 2 : we can even store the values in there. 
+  // as we are not setting up any initial value, so passing 'null'
+  const toogleButtonRef = useRef(null);
+  // now our ref is creted, we will attach it to DOM (JSX)
+
+  // once linking is done, now we can use this ref to do desired task. in our case we want it to get clicked
+  // toogleButtonRef.current.click();
+
+  // and we will get an error : "Uncaught TypeError: Cannot read property 'click' of null". this is bec, above function is getting executed before it this function is returning JSX where we are linking this 'toggleButtonRef' to button. thus, connection is not made to DOM, button is undefined, thus 'click' property cant be read when this function eexecutes.
+
+  // solution: putting the above statement in useEffect as useEffect will only run after return statement. 
+
 
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
 
-    // setTimeout(() => {
-    //   alert('Data saved to cloud');
-    // }, 1000);
+    // 
+    toogleButtonRef.current.click();
 
     return () => {
       console.log('[Cockpit.js] cleanup work in useEffect ');
@@ -42,6 +62,8 @@ const Cockpit = (props) => {
       <h1>{ props.title }</h1>
       <p className={assignedClasses.join(' ')}>This is really working.</p>
       <button  
+        // setting up ref
+        ref={toogleButtonRef}
         className={btnClass}
         onClick={props.clicked.bind(props.showPersons)}>Toggele Persons
       </button>
--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -26,33 +26,12 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
-  // lets say we want to foucus on last input element once the component has been rendered.
-
-  // 3 way
   constructor(props) {
     super(props)
-    // createRef : its a method offred on react object where we are importing and storing in input element.
-      // it can any object which react gives 
     this.inputElementRef = React.createRef();
   }
 
-
-
   componentDidMount() {
-    // // normal old JS way
-    // document.querySelector('input').focus();
-    // // this way is not preffred as it doesnt care if we are using react or not. once the component is rendered, it will check the DOM and do whatever task req.
-  
-    // // thus, react has easier to to select elements, a concept called 'ref' for 'references'
-
-
-    // // 2method
-    // this.inputElement.focus();
-    // // hence, this can only work in class based components.
-
-
-    // 3method: from 16.3+ we have one more way
-      // have to use current to access to current reference 
     this.inputElementRef.current.focus();
   }
   
@@ -66,26 +45,7 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input 
           key="i3"
-          // adding 'ref' special keyword, just like 'key' which will be understood and dected by react.
-          // there are 3 ways to setup ref
-          // 1way: supported in order versions
-            // passing anamous function which will get the arg pointing to that element. 
-            // we can name the arg what we want.
-          // ref= {(inputEl) => inputEl.focus()}
-          // as when component is rendered completely, our current element will be last element. thus, last input element will be focused. 
-
-
-          // 2nd method :  lets say we dont want to use that element here but somewhere else in our application
-            // creating a new property in class (global to class) which will hold the 'inputEl' ie current element.
-            // new property name can be anything, in our case its 'inputElement'
-            // 'componentDidMount' will run once all this set and there we can add foucus to this element
-          // ref={(inputEl) => {this.inputElement = inputEl}} 
-
-
-          // 3rd method:
           ref={this.inputElementRef}
-          // inputElementRef : it holds an access to this ref object. hence react will make the connection and this element will be reffered to inputElementRef
-
           type="text" 
           onChange={this.props.changed} 
           value={this.props.name} 
