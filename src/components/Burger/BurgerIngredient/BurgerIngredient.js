967f1f4a2e408f35076a24466aec90bd3f199481
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-12 17:10:09 +0530

    8.10. Adding Prop Type Validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,44 +1,94 @@
-// showing graphical burger ingrediants
+// we can use PropTypes in functional comp also the way we do in class based comp.
+// changung functional comp into class based
 
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './BurgerIngredient.css';
+import PropTypes from 'prop-types';
 
-const BurgerIngredient = (props) => {
-  // we will expect some props propties and based on that we will will be outputting burger ingredient.
+class BurgerIngredient extends Component {
+  render() {
+    let ingredient = null;
 
-  let ingredient = null;
+    switch(this.props.type) {
+      case ('bread-bottom') :
+        ingredient = <div className={classes.BreadBottom}></div>
+        break;
+      case ('bread-top') :
+        ingredient = (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>
+        );
+        break;
+      case ('meat') :
+        ingredient = <div className={classes.Meat}></div>
+        break;
+      case ('cheese') :
+        ingredient = <div className={classes.Cheese}></div>
+        break;
+      case ('salad') :
+        ingredient = <div className={classes.Salad}></div>
+        break;
+      case ('bacon') :
+        ingredient = <div className={classes.Bacon}></div>
+        break;
+      default :
+        ingredient = null;
+    }
 
-  // 'type' will be a property which will contain burger ingrediants.
-  switch(props.type) {
-    case ('bread-bottom') :
-      ingredient = <div className={classes.BreadBottom}></div>
-      break;
-    case ('bread-top') :
-      ingredient = (
-        <div className={classes.BreadTop}>
-          <div className={classes.Seeds1}></div>
-          <div className={classes.Seeds2}></div>
-        </div>
-      );
-      break;
-    case ('meat') :
-      ingredient = <div className={classes.Meat}></div>
-      break;
-    case ('cheese') :
-      ingredient = <div className={classes.Cheese}></div>
-      break;
-    case ('salad') :
-      ingredient = <div className={classes.Salad}></div>
-      break;
-    case ('bacon') :
-      ingredient = <div className={classes.Bacon}></div>
-      break;
-    default :
-      // if props.type doesnt mateches any of the string above
-      ingredient = null;
-  }
+    return ingredient;
+  };
+}
 
-  return ingredient;
+// adding propTypes
+BurgerIngredient.propTypes = {
+  // thus, type property must be of 'string' data type and is required.
+  // so, we try to use this component (BurgerIngrediant) withour passing 'type' property, will get an error
+  type: PropTypes.string.isRequired
 }
 
 export default BurgerIngredient;
+
+
+
+
+
+// import React from 'react';
+// import classes from './BurgerIngredient.css';
+
+// const BurgerIngredient = (props) => {
+//   let ingredient = null;
+
+//   switch(props.type) {
+//     case ('bread-bottom') :
+//       ingredient = <div className={classes.BreadBottom}></div>
+//       break;
+//     case ('bread-top') :
+//       ingredient = (
+//         <div className={classes.BreadTop}>
+//           <div className={classes.Seeds1}></div>
+//           <div className={classes.Seeds2}></div>
+//         </div>
+//       );
+//       break;
+//     case ('meat') :
+//       ingredient = <div className={classes.Meat}></div>
+//       break;
+//     case ('cheese') :
+//       ingredient = <div className={classes.Cheese}></div>
+//       break;
+//     case ('salad') :
+//       ingredient = <div className={classes.Salad}></div>
+//       break;
+//     case ('bacon') :
+//       ingredient = <div className={classes.Bacon}></div>
+//       break;
+//     default :
+//       ingredient = null;
+//   }
+
+//   return ingredient;
+// }
+
+// export default BurgerIngredient;
