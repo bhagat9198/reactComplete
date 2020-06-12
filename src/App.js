1f192456333d2c9d019de1ca0305463cae593452
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-12 13:11:48 +0530

    8.6. Setting up the Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// // importing class
+// import classes from './App.css';
+
+// function App() {
+//   return (
+//     // checking out if class module is working or not, by dynamically inserting the class
+//     <div className={classes.App}>
+//       <header className={classes.AppHeader}>
+//         <img src={logo} className={classes.App} alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://vuejs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+// deleting all the files(App.css and logo.svg) which are not req.
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
   );
 }
