55c7690c0c119ec5704ea28de201e66dd7307188
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-07-25 22:15:09 +0530

    12. Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,37 @@ import classes from './Burger.css';
 import BurgerIngrediant from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+  // getting ingrediants in props but as object. thus converting object to array
+  // 1. 
+    // keys methods : it extracts the keys of the given object and turns it into an array. thus gives the array of keys ie(salad, bacon, cheese, meat)
+  const transformedIngrediants = Object.keys(props.ingredients).map(igKey => {
+    // console.log(igKey); // salad, bacon, cheese, meat
+
+    // console.log(props.ingredient);
+    // console.log(props.ingredient.igKey);
+    // console.log(props.ingredient[igKey]);  // same output as above
+    // console.log(Array(props.ingredients[igKey]));
+    
+    // Array(length): its a method with creats an array of specific legth which is given as an argument. 
+    // Eg: Array(2) => [undefined, undefined];
+    
+    // using map on the formed array 
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      // console.log(i);
+      return <BurgerIngrediant key={igKey+i} type={igKey} />
+      });
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngrediant type='bread-top' />
-      <BurgerIngrediant type='salad' />
+      {/* <BurgerIngrediant type='salad' />
       <BurgerIngrediant type='meat' />
       <BurgerIngrediant type='cheese' />
-      <BurgerIngrediant type='bacon' />
+      <BurgerIngrediant type='bacon' /> */}
+
+      {/* passing the array */}
+      {transformedIngrediants}
       <BurgerIngrediant type='bread-bottom' />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import Auxilliary from '../../hoc/Auxilliary';
-// 
 import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
+  // inilizing the state
+  // 1
+  // constructor(props) {
+  //   super(props);
+  //   // as we are in the constructor, we need to use 'this' keyword to inilise the varaiable and here varaiable is our state.
+  //   this.state = {...}
+  // }
+
+  // 2
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 1
+    }
+  }
+
   render() {
     return (
       <Auxilliary>
-        {/* graphical burger */}
-        <Burger />
+        {/* passing ingrediants */}
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger controls</div>
       </Auxilliary>
     );
