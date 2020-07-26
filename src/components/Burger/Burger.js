0d7f6445e4713ff20ae0c97376a133a8247c8aa4
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-07-26 18:46:38 +0530

    8.13. Calculating the Ingredient Sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,40 +3,49 @@ import classes from './Burger.css';
 import BurgerIngrediant from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-  // getting ingrediants in props but as object. thus converting object to array
-  // 1. 
-    // keys methods : it extracts the keys of the given object and turns it into an array. thus gives the array of keys ie(salad, bacon, cheese, meat)
-  const transformedIngrediants = Object.keys(props.ingredients).map(igKey => {
-    // console.log(igKey); // salad, bacon, cheese, meat
+  // const transformedIngrediants = Object.keys(props.ingredients).map(igKey => {
+  //   return [...Array(props.ingredients[igKey])].map((_, i) => {
+  //     return <BurgerIngrediant key={igKey+i} type={igKey} />
+  //   });
+  // });
 
-    // console.log(props.ingredient);
-    // console.log(props.ingredient.igKey);
-    // console.log(props.ingredient[igKey]);  // same output as above
-    // console.log(Array(props.ingredients[igKey]));
-    
-    // Array(length): its a method with creats an array of specific legth which is given as an argument. 
-    // Eg: Array(2) => [undefined, undefined];
-    
-    // using map on the formed array 
+  // as now there are no ingredinats, it will be good to show some message to the user to add ingredants.
+  // console.log(transformedIngrediants); // [[],[],[],[]];
+  // transformedIngrediants is not null even if ingrediants are null. thus checking is length will not do the trick.
+
+  // thus, we can flatten this array by extracting all the values of inner arrays and making one single array
+  // this can be done by help of reduce() method : it a bulit in array function which allows us to transform an arrya to something else. it takes function as an input and it recives 2 args (callback and inital value)
+    // callback accepts 2 values automtically ie previous value and current value. 
+    // reduce method not only accept these callback here which is executed on every element in this array.
+    // it also accepts the inital value whcih can be anything. according to project, lets say an empty array
+  let transformedIngrediants = Object.keys(props.ingredients).map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
-      // console.log(i);
       return <BurgerIngrediant key={igKey+i} type={igKey} />
-      });
     });
+  }).reduce((arr, el) => {
+    // console.log(arr);
+    // console.log(el);
+    // so it will loop through all the elements. 
+    // we are concatingating the current value to initail element
+    return arr.concat(el);
+  },
+  // giving empty array as the initial value
+  []);
+
+  console.log(transformedIngrediants);
+  // now we will get a single array having jsx code inside if some ingrediants are added or an empty array. thus, now we can check transformedIngrediants array legth
+
+  if(transformedIngrediants.length === 0) {
+    transformedIngrediants = <p> Please start adding ingredants!</p>
+  } 
 
   return (
     <div className={classes.Burger}>
       <BurgerIngrediant type='bread-top' />
-      {/* <BurgerIngrediant type='salad' />
-      <BurgerIngrediant type='meat' />
-      <BurgerIngrediant type='cheese' />
-      <BurgerIngrediant type='bacon' /> */}
-
-      {/* passing the array */}
       {transformedIngrediants}
       <BurgerIngrediant type='bread-bottom' />
     </div>
   );
 }
 
-export default Burger;
+export default Burger;  
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,21 +3,14 @@ import Auxilliary from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-  // inilizing the state
-  // 1
-  // constructor(props) {
-  //   super(props);
-  //   // as we are in the constructor, we need to use 'this' keyword to inilise the varaiable and here varaiable is our state.
-  //   this.state = {...}
-  // }
 
-  // 2
+  // setting all the ingrediants to zero 
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 1
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
 
