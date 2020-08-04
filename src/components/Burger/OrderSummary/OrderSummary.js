039bb9b6740e2dd1772c5dd67aa756afbfe6a4f0
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-08-04 17:27:18 +0530

    8.37. Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxilliary from '../../../hoc/Auxilliary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients).map(igKey => {
-    return (
-    <li key={igKey}>
-      <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-    </li>
-    )
-  })
-  return (<Auxilliary>
-    <h3>Your Orders</h3>
-    <p>A delicious burger with the following ingredients:</p>
-    <ul>
-      {ingredientsSummary}
-    </ul>
-    <p><strong>Your total price: {props.price.toFixed(2)}</strong></p>
-    <p>Continue to checkout?</p>
-    <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-    <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-  </Auxilliary>);
+class OrderSummary extends Component {
+  // seeing when this component is getting updated
+  componentDidUpdate() {
+    console.log('Component Updated');
+  }
+  // and we can see that this compoenent is getting updated everytime when infrediant is added. thus, it should only be updated only when 'Order Now' btn is clicked ie when orderSummary componenet should be visible.
+  // and thus, this compoenent is controlled by Model compoenent.(from BurgerBuilder componenet we can see. OrderSummary resides in Modal)
+  // going to Modal
+
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+      <li key={igKey}>
+        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+      </li>
+      )
+    })
+    return (<Auxilliary>
+      <h3>Your Orders</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientsSummary}
+      </ul>
+      <p><strong>Your total price: {this.props.price.toFixed(2)}</strong></p>
+      <p>Continue to checkout?</p>
+      <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+    </Auxilliary>);
+  }
+  
 };
 
 export default OrderSummary;
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import Auxilliary from '../../../hoc/Auxilliary';
+// import Button from '../../UI/Button/Button';
+
+// const OrderSummary = (props) => {
+//   const ingredientsSummary = Object.keys(props.ingredients).map(igKey => {
+//     return (
+//     <li key={igKey}>
+//       <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+//     </li>
+//     )
+//   })
+//   return (<Auxilliary>
+//     <h3>Your Orders</h3>
+//     <p>A delicious burger with the following ingredients:</p>
+//     <ul>
+//       {ingredientsSummary}
+//     </ul>
+//     <p><strong>Your total price: {props.price.toFixed(2)}</strong></p>
+//     <p>Continue to checkout?</p>
+//     <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+//     <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+//   </Auxilliary>);
+// };
+
+// export default OrderSummary;
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Auxilliary from '../../../hoc/Auxilliary'
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const Modal = (props) => {
-  return <Auxilliary>
+class Modal extends Component {
+  // just for checking
+  componentDidUpdate() {
+    console.log('Component Updated');
+  }
+
+  componentWillUpdate() {
+    console.log('Component Will Update');
+  }
+
+  // putting condition
+  shouldComponentUpdate( nextProps, nextSate) {
+    return nextProps.show !== this.props.show;
+  }
+
+  // everything works fine and now model is not getting updated which meand OrderSummary is also not getting updated as it is within the Modal.
+  // we are not extending PureComponenet as we only want to check for only spaecific props ie 'show'. PureCompoenet will check for other other things too like clickListener in backdrop.But we do more checks within  shouldComponentUpdate, but our logic is straight forward, hence not needed.
+
+
+  render() {
+
+    return <Auxilliary>
     <Backdrop 
-      // 
-      show={props.show}
-      clicked={props.modalClosed} 
+      show={this.props.show}
+      clicked={this.props.modalClosed} 
     />
     <div 
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1': '0'
+        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: this.props.show ? '1': '0'
       }}
       className={classes.Modal}>
-      {props.children}
+      {this.props.children}
     </div>
   </Auxilliary>
+  }
+  
 }
 
 export default Modal;
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import classes from './Modal.css';
+// import Auxilliary from '../../../hoc/Auxilliary'
+// import Backdrop from '../Backdrop/Backdrop';
+
+
+// const Modal = (props) => {
+//   return <Auxilliary>
+//     <Backdrop 
+//       // 
+//       show={props.show}
+//       clicked={props.modalClosed} 
+//     />
+//     <div 
+//       style={{
+//         transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+//         opacity: props.show ? '1': '0'
+//       }}
+//       className={classes.Modal}>
+//       {props.children}
+//     </div>
+//   </Auxilliary>
+// }
+
+// export default Modal;
 
