a146ce53ed607992c3842b3ad58a10a93aa930fe
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-08-08 22:25:17 +0530

    11.15. Styling the Active Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import {Route, Link} from "react-router-dom"
+// import {Route, Link} from "react-router-dom";
+
+// as we inspect, we see that no class has been added on the active link. to do that, we need to import differt component which adds the class to active link and few extra porps and do exact same work as 'Link'
+import {Route, NavLink} from "react-router-dom"
+
 import "./Blog.css";
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -12,27 +16,37 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to={{
-                // whenever we write '/pathName', it means absolute path. ie, if our website is www.example.com, then our full path will be 'www.example.com/pathName' or according to our project 'localhost:3000/new-post'. 
-                // pathname: '/new-post',
-
-                // pathname: 'new-post',
-                // even if we write our url without '/', still it will be treated as 'www.example.com/new-post' and not 'www.example.com/posts/new-post' even if we were on 'www.example.com/posts'.
-                // hence, it will be always treated as absolute path
-
-                // relative path
-                // if we are on ''www.example.com/posts'(current path) and want to go on ''www.example.com/posts/new-post'.
-                // for that, we need to build the path dynamically and for that we can take helps of props
-                // we know ".props.match.url" will gives the current path.
-                // pathname: this.props.match.url + '/new-path',
-                // thus, its a realtive path as it takes the page we are currently on and build the futher path on that. 
-
-                // we will use absolute path here, as we dont have props
+              {/* replacing Link with NavLink */}
+              {/* now we can see, when we are on 'new-post' route at that time both the links are active ie colored with orange color. this is because '/new-post' use the prefix of '/' also. hence, both links are colored. this can be rectified by using 'exact' keyword so that it doesnt get used by '/new-post' */}
+              {/* <li><NavLink to="/">Home</NavLink></li> */}
+              {/* <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true'
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li> */}
+
+
+
+              {/* bydefault active link will get the class name 'active'. but if we want to change the, we can change it by  */}
+              <li><NavLink 
+                // giving own classname
+                activeClassName= "my-active"
+                // now styling will go off as our class anme has been changed and in css there is no styling for 'my-active'
+
+                // we can even style the active link here itself by:
+                activeStyle = {{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}
+                
+                exact 
+                to="/">Home</NavLink></li>
+              <li><NavLink to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
