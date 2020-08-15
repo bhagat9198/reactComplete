7b0a64f47fb7887ac5f5c331b9897ec313f4b831
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-08-15 20:39:37 +0530

    11.27. Working with Guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-// import {Route, NavLink, Switch} from "react-router-dom"
-// 
 import {Route, NavLink, Switch, Redirect} from "react-router-dom"
 import "./Blog.css";
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+  // state
+  state = {
+    auth: false
+  };
+
   render() {
     return (
       <div className="Blog">
@@ -30,19 +33,20 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/posts" component={Posts} />
-          <Route path="/new-post" component={NewPost} />
-          {/* rn, if we go to '/' except the navbar, we cant see anything. But we want all the posts should be visible */}
-          {/* 1.way */}
-          {/* <Route path="/" component={Posts} /> */}
+          {/* <Route path="/new-post" component={NewPost} /> */}
+          {/* if we want only authenticated user should able go throught NewPost. if we pass this route conditioanlly. */}
+          {/* hence, craeting a state for user and  and according to state passing teh route. */}
+          {/* using one line solution ie ternary operator or proper if block . using ternary */}
+          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
 
-          {/* 2.using Redirect component : its has 2 fields
-            from: url of user
-            to: where we want user to go
-            if we are using redirct in Switch componenent, we can use 'from' property. but if Redirect called outside of Switch component, then only 'to' property will be used.
-           */}
+          {/* when auth is false. after clicking on NewPost route. we will be redirected to the starting page.
+          why?
+          redirecting operation is taking place by Redirect comp. from="/" is taking everything which is not caught by anyother route above it and redircys to post. Hece, it is gaurd now */}
+
+          {/* 2nd way: NewPost */}
+
+          <Route path="/posts" component={Posts} />
           <Redirect from="/" to="/posts" />
-          {/* and automatically, '/' path will be converted into '/posts' */}
         </Switch>
       </div>
     );
