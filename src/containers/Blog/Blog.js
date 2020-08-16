16bb0ae483f19f4bf1b2ed6faa96b890364e1d3b
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-08-16 20:43:38 +0530

    11.30. Lazy Loading with React Suspense

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,73 +1,107 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+
+import React, { Component, Suspense } from "react";
+
 import {Route, NavLink, Switch, Redirect} from "react-router-dom"
 import "./Blog.css";
 import Posts from './Posts/Posts';
-// import NewPost from './NewPost/NewPost';
 
-// we want to load NewPost dynamically ie, only loading the code when user goes on NewPost route.
-// whenever we import lik this..import something from somewhere. 
-// import NewPost from './NewPost/NewPost';
-// we inform webpack, the build tool which gets usd behined the sceans about this dependency . eg './NewPost/NewPost'.
-// then it will be included in teh global bundle
+// no more using HOC to do lazy loading
+// import AsyncComponent from './../../hoc/AsyncComponent';
+// const AynsNewPost = AsyncComponent(() => {
+//   return import('./NewPost/NewPost');
+// });
 
-// but for lazy loading, it will be opposite, we dont want to include the code on global bundle. it should be loaded when needed.
-// for that webpack should able to dynamically prepare some extra bundle for the new component which will be loaded.
-// hence commenting out old way of importing
+// react 16.6 hads a new methods on react object, the lazy method which helps to load componants asynchronously. 
+// lazy method can be used with routing, till the time user doesnt visits the route, that componenent code will not be loaded.
+// it can be also used with conditions like if else 
 
-// importing AsyncComponent
-import AsyncComponent from './../../hoc/AsyncComponent';
-
-// executing AsyncComponent function component. it will require soem arg which should be a function so that it can executed within AsyncComponent component.
-// thus, passing anomous arrow function
-const AynsNewPost = AsyncComponent(() => {
-  // using import as function
-  // return import();
-  // this is a special syntax, teh dynamic import syntax which means whatever comes between the parenthese wll be only imported when warpping function is execued. 
-    // in our case, warppig function will be this anomous function. and this function will only be executed when we render AynsNewPost to dom.
-  
-  // hence, importing my NewPost component
-  return import('./NewPost/NewPost');
-});
+// again using dynamically importing our NewPost route with lazy method
+// lazy(): it accepts a function
+// dynamic import doesnt take named import. thus, default export is imporatnt here ie, export default something.
+const AynsNewPost = React.lazy(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
+
+  // 1.way
+  // state = {
+  //   auth: true
+  // };
+
+  // render() {
+  //   return (
+      
+  //       <div className="Blog">
+  //         <header>
+  //           <nav>
+  //             <ul>
+  //               <li><NavLink 
+  //                 activeClassName= "my-active"
+  //                 activeStyle = {{
+  //                   color: '#fa923f',
+  //                   textDecoration: 'underline'
+  //                 }}
+  //                 exact 
+  //                 to="/posts">Posts</NavLink></li>
+  //               <li><NavLink to={{
+  //                 pathname: '/new-post',
+  //                 hash: '#submit',
+  //                 search: '?quick-submit=true'
+  //               }}>New Post</NavLink></li>
+  //             </ul>
+  //           </nav>
+  //         </header>
+  //         <Switch>
+  //           {/* {this.state.auth ? <Route path="/new-post" component={AynsNewPost} /> : null} */}
+
+  //           {this.state.auth ? 
+  //             <Route path="/new-post" 
+  //               render={() => 
+  //                 // passing fallback prop, which is nessary. it will contain JSX code
+  //                 // it will be displayed when react postpond the reandering of this 'AynsNewPost' warpped componend. inplace of Loading, there can be spinner or something like that
+  //                 <Suspense fallback={<div>Loading...</div>}>
+  //                   <AynsNewPost />
+  //                 </Suspense>} /> 
+  //             : null}
+  //           <Route path="/posts" component={Posts} />
+  //           <Route render = {() => <h1 style={{textAlign: 'center'}}>Page Not Found</h1>} />
+  //         </Switch>
+  //       </div>
+      
+  //   );
+  // }
+
+
+  // 2.way
+  // react.lazy can be also be used without the help of Route.
+  
+  // creating a toggle button which will display Posts and NewPost alternately
   state = {
-    // making auth 'false' to 'true' so that we van navigate to NewPost
-    auth: true
+    posts: true
   };
 
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { posts: !prevState.posts}
+    })
+  }
+
   render() {
-    return (
-      <div className="Blog">
-        <header>
-          <nav>
-            <ul>
-              <li><NavLink 
-                activeClassName= "my-active"
-                activeStyle = {{
-                  color: '#fa923f',
-                  textDecoration: 'underline'
-                }}
-                exact 
-                to="/posts">Posts</NavLink></li>
-              <li><NavLink to={{
-                pathname: '/new-post',
-                hash: '#submit',
-                search: '?quick-submit=true'
-              }}>New Post</NavLink></li>
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-          {/* {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null} */}
-          {/* instaed of NewPost component, we will be using AynsNewPost component */}
-          {this.state.auth ? <Route path="/new-post" component={AynsNewPost} /> : null}
-          <Route path="/posts" component={Posts} />
-          <Route render = {() => <h1 style={{textAlign: 'center'}}>Page Not Found</h1>} />
-        </Switch>
-      </div>
+    return(
+      // it same like aux component, just like a wrapper. doesnt render some element to real dom and hence, doesnt distort our html
+      <React.Fragment >
+        <button onClick={this.modeHandler} >Toggle Posts</button>
+        {this.state.posts ? 
+          <h1>Post Page</h1> : 
+          <Suspense fallback={<div>Loading...</div>}>
+            <AynsNewPost />
+          </Suspense>
+        }
+      </React.Fragment>
     );
-    // thus, when we load NewPOst route, '1.chunk.js' will be created dynamically. this will be extra bundle which webpack is creating fro NewPost. but its not getting added to bundle.js as it prepared to laod when needed 
   }
 }
+
+// note: its is not supported for server side rendering. its only helpful for client side rendering.
 
 export default Blog;
