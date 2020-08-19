a39319dcf1b0aa8e655d2784d98f8641eefb349d
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-08-19 22:35:31 +0530

    14.9. Connecting the Store to React

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,41 +1,100 @@
-import React, { Component } from 'react';
+// rn, we are managing the state internally. now we will manage the state with redux-store. but the pattern of componenet will not changes. state is something which will be given by redux.
+// now, we need to add subscriptions. but this will be done different (not as we did in redux-basics)
 
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import React, { Component } from "react";
+// 1.
+// connect: its a function, a HOC wich will be used on export
+import { connect } from "react-redux";
+
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+  // state managed redux is not recieved as state here in componenet because this state is the thing which is changing internaly within this componenet.
+  state = {
+    counter: 0,
+  };
+  // thus, no redux will storing the state, not the componenet. and props are changed internally and hence mapping redux state with props. 
+  // hence we get the name 'mapStateToProps' 
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
+  counterChangedHandler = (action, value) => {
+    switch (action) {
+      case "inc":
+        this.setState((prevState) => {
+          return { counter: prevState.counter + 1 };
+        });
+        break;
+      case "dec":
+        this.setState((prevState) => {
+          return { counter: prevState.counter - 1 };
+        });
+        break;
+      case "add":
+        this.setState((prevState) => {
+          return { counter: prevState.counter + value };
+        });
+        break;
+      case "sub":
+        this.setState((prevState) => {
+          return { counter: prevState.counter - value };
+        });
+        break;
     }
+  };
 
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {/* outputing the store value */}
+        <CounterOutput value={this.props.ctrl} />
+        <CounterControl
+          label="Increment"
+          clicked={() => this.counterChangedHandler("inc")}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={() => this.counterChangedHandler("dec")}
+        />
+        <CounterControl
+          label="Add 5"
+          clicked={() => this.counterChangedHandler("add", 5)}
+        />
+        <CounterControl
+          label="Subtract 5"
+          clicked={() => this.counterChangedHandler("sub", 5)}
+        />
+      </div>
+    );
+  }
 }
 
-export default Counter;
+// 3.
+// to connect function, 2agrs are passed
+// 1. which part of appliaction we need. eg: in larger appliaction we will have lods of state and  specific container, doesnt needs all of the states, only few which that container is mamanging
+// 2. which actions we want to dispatch as in bigger appliactions, we can have lots of actions but giver container is will dispatching few actions only.
+
+// starting with state
+// mapStateToProps: storing instructions how the state managed by redux should be mapped to props.
+
+// it stors the function which expects the state stored in redux as the input(argument) and returns the js object which is a map of props names and slices of state stored in redux
+// 'state' as argument : it will be given by react-redux. so it will go to reducer.js file where we have have defined that state and extract it. 
+  // there we have initialSate which is having property 'counter'
+const mapStateToProps = state => {
+  return {
+    // defining our own props names
+    ctrl: state.counter
+
+  }
+}
+// this function will be executed by 'react-redux' package because we will pass it to. its our way of configuring which king of information we need.
+
+
+
+
+// 2.
+// export default Counter;
+
+// connect: its a function which returs the function  and this returned function takes a component as argument. so connect is not really a HOC, its a function which return the HOC.
+// thus, 'connect' itself can be called as function and since it returns the function. we then execute the result of connect of this function execution.
+
+export default connect(mapStateToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// including redux store
 import { createStore } from 'redux';
+// 
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// 
 import reducer from './store/reducer';
 
-// 1.
-// creating store
-// const store = createStore();
-// store will include reducer. as reducer will contain the logic for updating the store. its a convention to carete a seprate file. hence, cea=reating new file: store/reducer
-
-// 2.
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+// warpping our 'App' with Provider component.
+// its a helping component which helps to inject our store inro react components
+// thus, we need to pass our 'store' as props.
+// props name will be 'store', its predefined. 
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// thus redux store is connected to our application.
+// now, how to get the data from store? containers/Counter/Counter
 registerServiceWorker();
+
+
